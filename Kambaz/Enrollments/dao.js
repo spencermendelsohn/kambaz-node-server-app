9965835e337a4194b9d73c44154ff10bf0f85d75
Spencer Mendelsohn <spmendelsohn1@gmail.com>
2025-04-16 16:19:42 -0400

    a6-done

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,18 +1,22 @@
-import Database from "../Database/index.js";
-import { v4 as uuidv4 } from "uuid";
+import model from "./model.js";
 
-
-export function enrollUserInCourse(userId, courseId) {
-    const { enrollments } = Database;
-    enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
+export async function fetchEnrollments() {
+        return await model.find()
+            .populate("course")
+            .populate("user")
+            .exec();
+    }
+export async function findCoursesForUser(userId) {
+    const enrollments = await model.find({ user: userId }).populate("course");
+    return enrollments.map((enrollment) => enrollment.course);
 }
-export function unEnrollUserInCourse(userId, courseId) {
-    const { enrollments } = Database;
-    Database.enrollments = enrollments.filter(
-        (enrollment) => enrollment.user === userId && enrollment.course === courseId
-    );
+export async function findUsersForCourse(courseId) {
+    const enrollments = await model.find({ course: courseId }).populate("user");
+    return enrollments.map((enrollment) => enrollment.user);
 }
-
-export function fetchEnrollments() {
-    return Database.enrollments;
+export function enrollUserInCourse(user, course) {
+    return model.create({ user, course, _id: `${user}-${course}` });
+}
+export function unenrollUserFromCourse(user, course) {
+    return model.deleteOne({ user, course });
 }
